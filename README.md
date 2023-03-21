2d6bd6f2001ecc9836e643422cbc82b8784d624f
sahilsummit <sjaura@thesummitgrp.com>
2023-03-21 12:28:36 +0530

    vue-router

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vue Router 4 Source Code
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="nav">
+    <AppLink id="logo" to="/">Travel Places</AppLink>
+    <AppLink
+        v-for="destination in destinations"
+        :key="destination.id"
+        :to="{name: 'destination.show', params:{id: destination.id, slug: destination.slug}}"
+      >
+        {{ destination.name }}
+      </AppLink>
+      <AppLink :to="{name: 'protected'}">Dashboard</AppLink>
+  </div>
+</template>
+<script>
+import sourceData from '@/data.json'
+export default {
+  data(){
+    return {
+      destinations: sourceData.destinations
+    }
+  }
+}
+</script>
